e4e678b91e7a335c781cf3c75b3a3d380011f341
yukselsule <sulecantekin@gmail.com>
2024-07-02 15:01:20 +0300

    movies

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -6,18 +6,29 @@ import AddToList from "./AddToList";
 
 import "./MovieItem.scss";
 import Modal from "./Modal";
+import { useMovies } from "../contexts/MoviesContext";
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 function MovieItem({ movie }) {
+  const { movieDetails } = useMovies();
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [showAddToList, setShowAddToList] = useState(false);
+
+  const details = movieDetails[movie.id] || {};
+  const {
+    spoken_languages: languages = [],
+    runtime,
+    genres = [],
+    origin_country: countries = [],
+  } = details;
 
   const {
     title,
     release_date: released,
     poster_path: poster,
     overview,
+    vote_average: voted,
   } = movie;
 
   function handleAddToList() {
@@ -39,7 +50,27 @@ function MovieItem({ movie }) {
         />
         <div className="movie__details">
           <h2 className="movie__details-title">{title}</h2>
+          <span className="movie__details-voted"> {voted} </span>
+          <span className="movie__details-runtime">Runtime: {runtime} </span>
+          <p className="movie__details-languages">
+            Languages:
+            {languages.map((language) => {
+              return <span key={language.iso_639_1}>{language.name}</span>;
+            })}
+          </p>
           <span className="movie__details-released">{released}</span>
+          <p className="movie__details-genres">
+            Genres:
+            {genres.map((genre) => {
+              return <span key={genre.id}>{genre.name} </span>;
+            })}
+          </p>
+          <p className="movie__details-countries">
+            Countries:
+            {countries.map((country) => {
+              return <span key={country}>{country} </span>;
+            })}
+          </p>
           <p className="movie__details-overview">{overview}</p>
           <Button type="add" onClick={handleAddToList}>
             Add to list
--- src/contexts/MoviesContext.jsx
+++ src/contexts/MoviesContext.jsx
@@ -6,8 +6,25 @@ const BASE_URL = "https://api.themoviedb.org/3/";
 
 const MoviesContext = createContext();
 
+async function getMovieDetails(id) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMThkYzhmNjM2YWEyMDgyY2MxMDI5MzMyMWM2N2RkNSIsIm5iZiI6MTcxOTkxMTYzMy4xNDM1NzQsInN1YiI6IjY2N2U5YzZiOGExMTkxMTY4MjZkNDI3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QoIb9-KHmo3tRUnuf5C703-FVybJ1GODeah9lQiuoAs",
+      },
+    }
+  );
+  const data = await res.json();
+  return data;
+}
+
 function MoviesProvider({ children }) {
   const [movies, setMovies] = useState([]);
+  const [movieDetails, setMovieDetails] = useState({});
   const { query } = useSearchQuery();
 
   useEffect(
@@ -18,6 +35,14 @@ function MoviesProvider({ children }) {
         );
         const data = await res.json();
         setMovies(data.results);
+
+        data.results.forEach(async (movie) => {
+          const details = await getMovieDetails(movie.id);
+          setMovieDetails((prevDetails) => ({
+            ...prevDetails,
+            [movie.id]: { ...movie, ...details },
+          }));
+        });
       }
       getMovie();
     },
@@ -25,7 +50,7 @@ function MoviesProvider({ children }) {
   );
 
   return (
-    <MoviesContext.Provider value={{ movies }}>
+    <MoviesContext.Provider value={{ movies, movieDetails }}>
       {children}
     </MoviesContext.Provider>
   );
