562a74972ca599b4a4a310ff0d7b4e50824015e0
yukselsule <sulecantekin@gmail.com>
2024-07-04 12:32:19 +0300

    Spinner Component added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import "./Spinner.scss";
+
+function Spinner() {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
