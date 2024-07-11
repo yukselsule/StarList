922dd9f25401956028715d42fdd869d69d8fca05
yukselsule <sulecantekin@gmail.com>
2024-07-11 14:34:23 +0300

    BackgroundVideo component added

---
+++ src/components/BackgroundVideo.jsx
@@ -0,0 +1,15 @@
+import bgvideo from "../assets/img/bgvideo.mp4";
+import styles from "./BackgroundVideo.module.scss";
+
+function BackgroundVideo() {
+  return (
+    <div className={styles.bg}>
+      <video autoPlay loop muted className={styles["bg-video"]}>
+        <source src={bgvideo} type="video/mp4" />
+      </video>
+      <div className={styles["bg-overlay"]}></div>
+    </div>
+  );
+}
+
+export default BackgroundVideo;
