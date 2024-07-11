e4f9cd04ef90edbd4ba36ce0e5e3b079b2566322
yukselsule <sulecantekin@gmail.com>
2024-07-11 14:36:46 +0300

    Global scss

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/variables.scss";`,
+        additionalData: `@import "./src/styles/general.scss";`,
       },
     },
   },
