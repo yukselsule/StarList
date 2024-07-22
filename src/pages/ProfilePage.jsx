75aadcc4e53df7d2c6594b59b582acf7a2e38b1c
yukselsule <sulecantekin@gmail.com>
2024-07-22 16:40:58 +0300

    ProfilePage style

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,16 +1,33 @@
+import { useLists } from "../contexts/ListsContext";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Lists from "../components/Lists";
 import Summary from "../components/Summary";
 
+import styles from "./ProfilePage.module.scss";
+
 function ProfilePage() {
+  const { movieDetails } = useLists();
   return (
     <div>
       <Header />
-      <div className="profile-page">
-        <Summary />
-        <Lists />
-      </div>
+      <main className={styles["profile-page"]}>
+        {movieDetails.length === 0 && (
+          <h2 className={styles["profile-page__empty"]}>
+            {" "}
+            Go watch some movies to make lists!{" "}
+          </h2>
+        )}
+
+        {movieDetails.length > 0 && (
+          <div className={styles["profile-page__full"]}>
+            <Summary />
+            <Lists />
+          </div>
+        )}
+      </main>
+
       <Footer />
     </div>
   );
