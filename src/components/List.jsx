806f18e37dd774192088dacfa1f619b5873ff8c6
yukselsule <sulecantekin@gmail.com>
2024-08-06 17:58:13 +0300

    framer motions

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { useLists } from "../contexts/ListsContext.jsx";
 
@@ -22,6 +23,30 @@ function List() {
     deleteList(listId, handleNavigate);
   }
 
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, translateY: 20 },
+    visible: {
+      opacity: 1,
+      translateY: 0,
+      transition: { duration: 0.5, ease: "easeInOut" },
+    },
+    exit: {
+      opacity: 0,
+      height: 0,
+      overflow: "hidden",
+      transition: { duration: 0.5, ease: "easeInOut" },
+    },
+  };
+
   return (
     <div className={styles["list"]}>
       <div className={styles["list-heading"]}>
@@ -31,13 +56,72 @@ function List() {
         </Button>
       </div>
 
-      <ul>
-        {movies.map((movie, index) => (
-          <ListItem movie={movie} key={index} listName={listId} />
-        ))}
-      </ul>
+      <motion.ul
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className={styles["lists-container"]}
+      >
+        <AnimatePresence>
+          {movies.map((movie) => (
+            <motion.li
+              key={movie.id}
+              variants={itemVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <ListItem movie={movie} listName={listId} />
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </motion.ul>
     </div>
   );
 }
 
 export default List;
+
+// import { useNavigate, useParams } from "react-router-dom";
+
+// import { useLists } from "../contexts/ListsContext.jsx";
+
+// import Button from "./Button.jsx";
+// import ListItem from "./ListItem.jsx";
+
+// import styles from "./List.module.scss";
+
+// function List() {
+//   const { lists, deleteList } = useLists();
+//   const { listId } = useParams();
+//   const movies = lists[listId] || [];
+
+//   const navigate = useNavigate();
+
+//   function handleNavigate() {
+//     navigate(-1);
+//   }
+
+//   function handleDeleteList() {
+//     deleteList(listId, handleNavigate);
+//   }
+
+//   return (
+//     <div className={styles["list"]}>
+//       <div className={styles["list-heading"]}>
+//         <h2>{listId}</h2>
+//         <Button type="delete" onClick={handleDeleteList}>
+//           Delete list
+//         </Button>
+//       </div>
+
+//       <ul>
+//         {movies.map((movie, index) => (
+//           <ListItem movie={movie} key={index} listName={listId} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default List;
