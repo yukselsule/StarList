4505205b491d343d8ba66fedbedd72d83f4f3f39
yukselsule <sulecantekin@gmail.com>
2024-08-06 17:58:20 +0300

    framer motions

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { useError } from "../contexts/ErrorContext";
 import { useMovies } from "../contexts/MoviesContext";
 
@@ -12,18 +14,36 @@ function MovieList() {
   const { movies, isLoading } = useMovies();
   const sortedMovies = [...movies].sort((a, b) => b.popularity - a.popularity);
 
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, translateY: 15 },
+    visible: {
+      opacity: 1,
+      translateY: 0,
+    },
+  };
+
   if (isLoading) return <SpinnerFullPage />;
 
   if (error) return <Error error={error} />;
 
   return (
-    <div>
+    <motion.div initial="hidden" animate="visible" variants={container}>
       <ul className={styles["movie-list"]}>
         {sortedMovies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
+          <motion.div key={movie.id} variants={item}>
+            <MovieCard movie={movie} />
+          </motion.div>
         ))}
       </ul>
-    </div>
+    </motion.div>
   );
 }
 
