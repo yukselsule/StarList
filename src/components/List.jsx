daf5aa76d631599363e63706a03ca33576bc5a7a
yukselsule <sulecantekin@gmail.com>
2024-08-07 14:25:03 +0300

    List Framer motion

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,6 +8,30 @@ import ListItem from "./ListItem.jsx";
 
 import styles from "./List.module.scss";
 
+const containerVariants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, translateY: 20 },
+  visible: {
+    opacity: 1,
+    translateY: 0,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+  exit: {
+    opacity: 0,
+    height: 0,
+    overflow: "hidden",
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+};
+
 function List() {
   const { lists, deleteList } = useLists();
   const { listId } = useParams();
@@ -22,30 +46,6 @@ function List() {
   function handleDeleteList() {
     deleteList(listId, handleNavigate);
   }
-
-  const containerVariants = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.1,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { opacity: 0, translateY: 20 },
-    visible: {
-      opacity: 1,
-      translateY: 0,
-      transition: { duration: 0.5, ease: "easeInOut" },
-    },
-    exit: {
-      opacity: 0,
-      height: 0,
-      overflow: "hidden",
-      transition: { duration: 0.5, ease: "easeInOut" },
-    },
-  };
 
   return (
     <div className={styles["list"]}>
@@ -81,47 +81,3 @@ function List() {
 }
 
 export default List;
-
-// import { useNavigate, useParams } from "react-router-dom";
-
-// import { useLists } from "../contexts/ListsContext.jsx";
-
-// import Button from "./Button.jsx";
-// import ListItem from "./ListItem.jsx";
-
-// import styles from "./List.module.scss";
-
-// function List() {
-//   const { lists, deleteList } = useLists();
-//   const { listId } = useParams();
-//   const movies = lists[listId] || [];
-
-//   const navigate = useNavigate();
-
-//   function handleNavigate() {
-//     navigate(-1);
-//   }
-
-//   function handleDeleteList() {
-//     deleteList(listId, handleNavigate);
-//   }
-
-//   return (
-//     <div className={styles["list"]}>
-//       <div className={styles["list-heading"]}>
-//         <h2>{listId}</h2>
-//         <Button type="delete" onClick={handleDeleteList}>
-//           Delete list
-//         </Button>
-//       </div>
-
-//       <ul>
-//         {movies.map((movie, index) => (
-//           <ListItem movie={movie} key={index} listName={listId} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default List;
