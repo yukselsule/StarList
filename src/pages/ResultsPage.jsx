a5444c7607e432db762114de86aa2362dc3572d4
yukselsule <sulecantekin@gmail.com>
2024-08-09 16:34:50 +0300

    Pagination added to Results Page

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,23 +1,29 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
+import { useMovies } from "../contexts/MoviesContext";
 import { useSearchQuery } from "../contexts/SearchQueryContext";
 
 import MovieList from "../components/MovieList";
+import Pagination from "../components/Pagination";
+
+import styles from "./ResultsPage.module.scss";
 
 function ResultsPage() {
   const [searchParams] = useSearchParams();
   const searchQuery = searchParams.get("query") || "";
 
   const { setQuery } = useSearchQuery();
+  const { totalPages } = useMovies();
 
   useEffect(() => {
     setQuery(searchQuery);
   }, [searchQuery, setQuery]);
 
   return (
-    <div className="container">
+    <div className={`container ${styles["results-page"]}  `}>
       <MovieList />
+      <Pagination totalPages={totalPages} />
     </div>
   );
 }
